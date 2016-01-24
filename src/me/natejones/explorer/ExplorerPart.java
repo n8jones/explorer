dd279ce803c41ef4f36be86fb54c7dd6e6c255be
Nate Jones <n8jones@gmail.com>
2016-01-24 14:32:16 -0500

    Adding file size, type, and icon columns.  Adding the ability to go by typing enter in the address box.  Adding an up button to get parent folders.  Adding the ability to open files within eclipse.

--- src/me/natejones/explorer/ExplorerPart.java
+++ src/me/natejones/explorer/ExplorerPart.java
@@ -1,7 +1,6 @@
 
 package me.natejones.explorer;
 
-import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -12,55 +11,103 @@ import java.util.stream.StreamSupport;
 
 import javax.annotation.PostConstruct;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.e4.ui.di.PersistState;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swt.FXCanvas;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 
 public class ExplorerPart {
+	private static final String PERSIST_KEY =
+			ExplorerPart.class.getName() + ".path";
 	private final ObservableList<Item> items =
 			FXCollections.observableArrayList();
 	private TextField pathCtl;
 
+	@SuppressWarnings("unchecked")
 	@PostConstruct
 	public void postConstruct(Composite parent, MPart part) throws IOException {
 		parent.setLayout(new FillLayout());
 		FXCanvas canvas = new FXCanvas(parent, SWT.BORDER);
 		GridPane grid = new GridPane();
+		Button upBtn = new Button("Up");
+		upBtn.setOnAction(e -> {
+			Path p = Paths.get(pathCtl.getText());
+			p = p.getParent();
+			pathCtl.setText(p.toString());
+			go();
+		});
 		pathCtl = new TextField();
-		grid.add(pathCtl, 1, 1);
 		GridPane.setHgrow(pathCtl, Priority.ALWAYS);
+		pathCtl.setOnAction(e -> go());
 		Button goBtn = new Button("Go");
 		goBtn.setOnMouseClicked(e -> go());
-		grid.add(goBtn, 2, 1);
+
+		grid.addRow(1, upBtn, pathCtl, goBtn);
+
 		TableView<Item> table = new TableView<>(items);
 		table.setOnMouseClicked(e -> {
 			if (e.getClickCount() != 2)
 				return;
 			Item i = table.getSelectionModel().getSelectedItem();
-			pathCtl.setText(i.getName());
+			pathCtl.setText(i.getPath().toString());
 			go();
 		});
 		TableColumn<Item, String> nameColumn = new TableColumn<>("Name");
 		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-		table.getColumns().add(nameColumn);
-		grid.add(table, 1, 2, 2, 1);
+		nameColumn.setPrefWidth(200);
+		TableColumn<Item, Image> iconColumn = new TableColumn<>();
+		iconColumn.setCellValueFactory(new PropertyValueFactory<>("icon"));
+		iconColumn.setCellFactory(c -> new TableCell<Item, Image>() {
+			@Override
+			protected void updateItem(Image item, boolean empty) {
+				ImageView img = new ImageView(item);
+				img.setImage(item);
+				setGraphic(img);
+			}
+		});
+		iconColumn.setPrefWidth(25);
+		TableColumn<Item, String> typeColumn = new TableColumn<>("Type");
+		typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+		TableColumn<Item, Long> sizeColumn = new TableColumn<>("Size");
+		sizeColumn.setCellValueFactory(new PropertyValueFactory<>("size"));
+		table.getColumns().addAll(iconColumn, nameColumn, sizeColumn, typeColumn);
+		grid.add(table, 0, 2, 3, 1);
 		GridPane.setVgrow(table, Priority.ALWAYS);
 		Scene scene = new Scene(grid);
 		canvas.setScene(scene);
+
+		String persisted = part.getPersistedState().get(PERSIST_KEY);
+		if (persisted != null)
+			pathCtl.setText(persisted);
+
 		go();
+	}
+
+	@PersistState
+	public void persistState(MPart part) {
+		part.getPersistedState().put(PERSIST_KEY, pathCtl.getText());
 	}
 
 	private void go() {
@@ -73,15 +120,23 @@ public class ExplorerPart {
 						false);
 			else {
 				Path p = Paths.get(pstr).toRealPath().toAbsolutePath();
-				if (!Files.isDirectory(p))
+				if (!Files.isDirectory(p)) {
+					IFileStore fileStore =
+							EFS.getLocalFileSystem().getStore(p.toUri());
+					IWorkbenchPage page = PlatformUI.getWorkbench()
+							.getActiveWorkbenchWindow().getActivePage();
+					IDE.openEditorOnFileStore(page, fileStore);
+					pathCtl.setText(p.getParent().toString());
+					go();
 					return;
+				}
 				pathCtl.setText(p.toString());
 				newItems = Files.list(p);
 			}
 			items.clear();
 			newItems.map(Item::new).forEach(items::add);
 		}
-		catch (IOException e) {
+		catch (IOException | PartInitException e) {
 			throw new RuntimeException(e);
 		}
 	}
--- src/me/natejones/explorer/Item.java
+++ src/me/natejones/explorer/Item.java
@@ -1,24 +1,53 @@
 package me.natejones.explorer;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
+
+import javax.swing.Icon;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
 
 public class Item {
-	private String name;
+	private final Path path;
 
-	public Item(Path p) {
-		name = p.toString();
+	public Item(Path path) {
+		this.path = Objects.requireNonNull(path, "path must not be null");
+	}
+
+	public Path getPath() {
+		return path;
 	}
 
 	public String getName() {
-		return name;
+		Path fn = path.getFileName();
+		return fn == null ? path.toString() : fn.toString();
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public Long getSize() throws IOException {
+		return Files.isRegularFile(path) ? Files.size(path) : null;
+	}
+
+	public String getType() throws IOException {
+		return Files.probeContentType(path);
+	}
+
+	public Image getIcon() {
+		Icon icon = javax.swing.filechooser.FileSystemView.getFileSystemView()
+				.getSystemIcon(path.toFile());
+		BufferedImage img = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = img.createGraphics();
+		icon.paintIcon(null, graphics, 0, 0);
+	   graphics.dispose();
+		return SwingFXUtils.toFXImage(img, null);
 	}
 
 	@Override
 	public String toString() {
-		return String.format("Item [name=%s]", name);
+		return String.format("Item [%s]", path);
 	}
 }
